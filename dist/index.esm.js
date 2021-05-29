04d0f8a24458c56fbce2a6ec1c13ed3d8e4bb020
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-29 04:50:25 -0300

    Feat: update color

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2730,7 +2730,7 @@ var useKonamiCheatCode = function (matchedCodeHandler) {
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#1FC7D4",
+    primary: "#3BB7FF",
     primaryBright: "#53DEE9",
     primaryDark: "#0098A1",
     secondary: "#5BB9FF",
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,7 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#1FC7D4",
+  primary: "#3BB7FF",
   primaryBright: "#53DEE9",
   primaryDark: "#0098A1",
   secondary: "#5BB9FF",
